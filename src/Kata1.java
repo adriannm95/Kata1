2a3123d1a79011f10b7d0acce84690bc099b0fff
adrian <adrichicha@gmail.com>
2018-09-24 22:07:21 +0100

    Calendar Version

--- src/Kata1.java
+++ src/Kata1.java
@@ -1,7 +1,11 @@
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 public class Kata1 {
     public static void main(String[] args){
-        Person p = new Person( "Adrian Negrin",new Date(95,7,7));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(1995,7,7);
+        Person p = new Person( "Adrian Negrin",date);
         System.out.println(p.getName() + " tiene actualmente "+ p.getAge());
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,18 +1,31 @@
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Person {
     private final String name;
-    private final Date birth;
-    public Person(String name, Date birth){
+    private final Calendar birth;
+    private final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
+
+    public Person(String name, Calendar birth){
         this.name = name;
         this.birth = birth;
+
     }
+
     public String getName(){
         return name;
     }
-    public Date getBirth(){
+
+    public Calendar getBirth(){
         return birth;
     }
+
     public int getAge(){
-        return (int) ((new Date().getTime() - birth.getTime())/31536000000L);
+        Calendar today = GregorianCalendar.getInstance();
+        return (int) (milliSecondsToYear(today.getTimeInMillis() - birth.getTimeInMillis()));
+    }
+
+    private long milliSecondsToYear(long milles){
+        return milles / MILLISECONDS_PER_YEAR;
     }
 }
